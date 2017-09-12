a9200759816f064e1a4ca4591be2ea1b41297c62
root <yanwenze@tj-fintech.com>
2017-09-12 14:38:32 +0800

    test

--- sm2/key.go
+++ sm2/key.go
@@ -346,7 +346,7 @@ func marshalSm2EcryptedPrivateKey(PrivKey *PrivateKey, pwd []byte) ([]byte, erro
 		Pkdf2Params{
 			salt,
 			iter,
-			AlgorithmIdentifier{
+			asn1.AlgorithmIdentifier{
 				oidSHA1,
 				asn1.RawValue{
 					Tag:        5,
