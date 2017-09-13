33d0cb2c5dd38c71362d07a3f2603d64d3ec9870
root <yanwenze@tj-fintech.com>
2017-09-13 13:28:08 +0800

    sm4密钥文件pem实现(包括加密和不加密版本)

--- sm4/sm4_test.go
+++ sm4/sm4_test.go
@@ -17,13 +17,17 @@ package sm4
 
 import (
 	"fmt"
+	"log"
 	"testing"
 )
 
 func TestSM4(t *testing.T) {
 	data := []byte{0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10}
-	WriteKeyToPem("key.pem", data, nil)
-	key, _ := ReadKeyFromPem("key.pem", nil)
+	WriteKeyToPem("key.pem", data, []byte("123"))
+	key, err := ReadKeyFromPem("key.pem", []byte("123"))
+	if err != nil {
+		log.Fatal(err)
+	}
 	d0 := make([]byte, 16)
 	EncryptBlock(key, d0, data)
 	fmt.Printf("d0 = %x\n", d0)
