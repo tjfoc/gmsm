c3ff2589f744d34a3d3b6987b1257fd2e7a0f629
GongShengzhi <gongshengzhi@tj-fintech.com>
2019-02-18 14:40:23 +0800

    -- 国密签名实现pkcs7格式

--- sm2/sm2_test.go
+++ sm2/sm2_test.go
@@ -199,21 +199,22 @@ func TestSm2(t *testing.T) {
 
 func BenchmarkSM2(t *testing.B) {
 	t.ReportAllocs()
+	msg := []byte("test")
+	priv, err := GenerateKey() // 生成密钥对
+	if err != nil {
+		log.Fatal(err)
+	}
+	t.ResetTimer()
 	for i := 0; i < t.N; i++ {
-		priv, err := GenerateKey() // 生成密钥对
-		if err != nil {
-			log.Fatal(err)
-		}
-		msg := []byte("test")
 		sign, err := priv.Sign(rand.Reader, msg, nil) // 签名
 		if err != nil {
 			log.Fatal(err)
 		}
-		ok := priv.Verify(msg, sign) // 密钥验证
-		if ok != true {
-			fmt.Printf("Verify error\n")
-		} else {
-			fmt.Printf("Verify ok\n")
-		}
+		priv.Verify(msg, sign) // 密钥验证
+		// if ok != true {
+		// 	fmt.Printf("Verify error\n")
+		// } else {
+		// 	fmt.Printf("Verify ok\n")
+		// }
 	}
 }
